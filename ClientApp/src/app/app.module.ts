1691aa25e56e528ca050c83daa1ba02fa663fab2
Boza33 <mbozovic4521it@raf.rs>
2024-05-19 23:03:02 +0200

    HomePage, MainForm
    Frontend: Uradjena pocetna strana, header i footer, kao i glavna forma.
    
    Backend: Metode za pokretanje, slanje i validaciju forme.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -5,7 +5,7 @@ namespace Chat_App.Dtos
     public class UserDto
     {
         [Required]
-        [StringLength(15, MinimumLength = 3, ErrorMessage = "Ime mora imati vise od 2 karaktera.")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Ime mora imati minimum {2} i maksimum {1} karaktera.")]
         public string Name { get; set; }
     }
 }
