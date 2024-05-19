bb072a2c9f29fb014de779ddfb84d8cbd4535282
Boza33 <mbozovic4521it@raf.rs>
2024-05-19 13:27:56 +0200

    Kreiranje projekta.

---
+++ Services/ChatServices.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chat_App.Services
+{
+    public class ChatServices
+    {
+
+        private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
+        public bool AddUserToList(string userToAdd)
+        {
+            lock (Users)
+            {
+                foreach(var user in Users)
+                {
+                    if (user.Key.ToLower() == userToAdd.ToLower())
+                        return true;
+                }
+            }
+            Users.Add(userToAdd, null);
+            return true;  
+        }
+        public void AddUserConnectionID(string user, string connectionID)
+        {
+            lock (Users)
+            {
+                if (Users.ContainsKey(user))
+                    Users[user] = connectionID;
+            }
+        }
+        
+    }
+}
